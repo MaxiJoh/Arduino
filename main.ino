044f33bd1ae9b3656e96920311cfdcd3c3e796b2
MaxiJoh <maxi.johannink@gmail.com>
2023-07-17 17:36:09 +0200

    code to change frequency of pwm

--- main.ino
+++ main.ino
@@ -1,5 +1,20 @@
 #include "PWM/PWM.h"
+//#include "Arduino.h"
+//#include "foo.h"
+
+// C++ code
+//
+
+int led = 11;
+int32_t frequency = 1000;
 
 void setup() {
+  InitTimersSafe();
+
+  bool success = SetPinFrequencySafe(led, frequency);
+
+}
+
+void loop() {
 
 }
